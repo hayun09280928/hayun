2577b2046388c794ab0221fb991eae5ad25a0653
hayun09280928 <hayun35587438@gmail.com>
2026-07-20 10:41:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.markdown("AI 챗봇 만들 ")
-st.markdown("---"
+st.markdown("---")
 st.markdown("## 질문을 하시면 AI 친구가응답합니다.")         
 st.markdown("* **제작 언어**: 'Python'")
 st.markdown("* **프레임워크**: 'streamlit'")
