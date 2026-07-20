50e40c01610eaab1bf3963db614bfbbf7ce42efc
hayun09280928 <hayun35587438@gmail.com>
2026-07-20 11:17:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.title("앱 UI 만들기")
 name = st.text_input("이름")
-grade = st.radio("학년", [1,2,3], horizontal=True)
+grade = st.radio("학년", [1, 2, 3], horizontal=True)
 cls = st.number_input("반", value=1)
-level = st.select_slider("난이도", ["쉬움","중간","어려움"], value = "보통")
-score = st.slider("점수", 0,100, 50)
+level = st.select_slider("난이도", ["쉬움", "보통", "어려움"], value="보통")
+score = st.slider("점수", 0, 100, 50)
 text = st.text_area("소감")
 
 if st.button("확인"):
     st.success(f"{name} / {grade}학년 / {cls}반 / {level}")
-    st.markdown(f"점수: '{score}'")
+    st.markdown(f"점수: `{score}`")
     st.info(f"소감: {text}")
