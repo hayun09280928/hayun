49906db7720835b9842376000cd4b9cf5b2c6444
hayun09280928 <hayun35587438@gmail.com>
2026-07-21 09:25:47 +0900

    Update app-5.py

--- app-5.py
+++ app-5.py
@@ -59,9 +59,9 @@ def page3():
             st.session_state.todo_list = []
             st.rerun()
 pg= st.navigation([
-    st.Page(page_motto, title="오늘의 다짐", icon="📣"),
-    st.Page(page_motto, title="오늘의 할 일", icon="✅"),
-    st.Page(page_motto, title="나의 갓생 지수 ", icon="📈"),
+    st.Page(page1, title="오늘의 다짐", icon="📣"),
+    st.Page(page2, title="오늘의 할 일", icon="✅"),
+    st.Page(page3, title="나의 갓생 지수 ", icon="📈")
     
 st.title("🌱 갓생 살기 플래너")
 pg.run()
